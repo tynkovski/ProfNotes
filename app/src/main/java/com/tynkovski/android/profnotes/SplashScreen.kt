863c9b4cd18f7631791eb8fc69322fa903f0684a
Stanislav <tynkovski@gmail.com>
2022-07-17 18:53:57 +0400

    Added SplashScreen

---
+++ app/src/main/java/com/tynkovski/android/profnotes/SplashScreen.kt
@@ -0,0 +1,16 @@
+package com.tynkovski.android.profnotes
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
