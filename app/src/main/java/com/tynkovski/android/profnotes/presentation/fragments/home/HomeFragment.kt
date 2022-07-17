7ee5a7f528bd46e64e1709a31a2222813ed33a8e
Stanislav <tynkovski@gmail.com>
2022-07-17 22:20:23 +0400

    Fixed binding

--- app/src/main/java/com/tynkovski/android/profnotes/presentation/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/tynkovski/android/profnotes/presentation/fragments/home/HomeFragment.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import by.kirich1409.viewbindingdelegate.viewBinding
 import com.tynkovski.android.profnotes.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
--- app/src/main/java/com/tynkovski/android/profnotes/presentation/fragments/preferences/PreferencesFragment.kt
+++ app/src/main/java/com/tynkovski/android/profnotes/presentation/fragments/preferences/PreferencesFragment.kt
@@ -29,4 +29,9 @@ class PreferencesFragment : Fragment() {
         }
         return root
     }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
