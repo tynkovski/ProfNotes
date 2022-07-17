b08b38ffae7d4a83d8eb658eefd4335f54bfaa10
Stanislav <tynkovski@gmail.com>
2022-07-17 18:23:43 +0400

    Updated MainActivity.kt

--- app/src/main/java/com/tynkovski/android/profnotes/MainActivity.kt
+++ app/src/main/java/com/tynkovski/android/profnotes/MainActivity.kt
@@ -4,22 +4,18 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.tynkovski.android.profnotes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val navigationView get() = binding.bottomNavigation
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.bottomNavigation
-
-        val navController = findNavController(R.id.navigationHost)
-        navView.setupWithNavController(navController)
+        navigationView.setupWithNavController(
+            findNavController(R.id.navigationHost)
+        )
     }
 }
