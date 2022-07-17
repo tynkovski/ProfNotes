6ad86f7a0754c6fa5dee85fa333e93d362f27965
tynkovski <tynkovski@gmail.com>
2022-07-17 21:47:58 +0400

    View model update

--- app/src/main/java/com/tynkovski/android/profnotes/presentation/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/tynkovski/android/profnotes/presentation/fragments/home/HomeFragment.kt
@@ -6,34 +6,26 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.tynkovski.android.profnotes.databinding.FragmentHomeBinding
+import com.tynkovski.android.profnotes.databinding.FragmentPreferencesBinding
 
 class HomeFragment : Fragment() {
-
-    private var _binding: FragmentHomeBinding? = null
-    private val binding get() = _binding!!
+    private val binding by viewBinding(FragmentHomeBinding::bind)
+    private val viewModel: HomeViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
         val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
+        viewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 }
--- app/src/main/java/com/tynkovski/android/profnotes/presentation/fragments/preferences/PreferencesFragment.kt
+++ app/src/main/java/com/tynkovski/android/profnotes/presentation/fragments/preferences/PreferencesFragment.kt
@@ -6,29 +6,25 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.tynkovski.android.profnotes.databinding.FragmentPreferencesBinding
 
 class PreferencesFragment : Fragment() {
     private val binding by viewBinding(FragmentPreferencesBinding::bind)
+    private val viewModel: PreferencesViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val preferencesViewModel = ViewModelProvider(this).get(PreferencesViewModel::class.java)
         val root: View = binding.root
-
         val textView: TextView = binding.textPreferences
-        preferencesViewModel.text.observe(viewLifecycleOwner) {
+        viewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
     }
 }
