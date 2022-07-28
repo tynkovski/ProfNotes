5caec0b8db761e821d71f28847ea40997f327242
Stanislav <tynkovski@gmail.com>
2022-07-29 02:55:55 +0400

    Code beautified

--- app/src/main/java/com/tynkovski/android/profnotes/presentation/splash/SplashScreen.kt
+++ app/src/main/java/com/tynkovski/android/profnotes/presentation/splash/SplashScreen.kt
@@ -2,8 +2,8 @@ package com.tynkovski.android.profnotes.presentation.splash
 
 import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.tynkovski.android.profnotes.presentation.main.MainActivity
 
 @SuppressLint("CustomSplashScreen")
